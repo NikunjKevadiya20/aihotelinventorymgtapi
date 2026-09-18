f13ee56697379add0152f371a6830a092da7c311
Bhagirathsinh Mori <Bhagirath.aseum@gmail.com>
2026-09-18 16:06:12 +0530

    changes done by priyam

--- HotelBookingAPI/Helpers/TenantMiddleware.cs
+++ HotelBookingAPI/Helpers/TenantMiddleware.cs
@@ -1,92 +1,4 @@
-﻿//using System.Data.SqlClient;
-//using System.Text.Json;
-//using Dapper;
-//using HotelBooking.DataAccess.Base;
-//using HotelBooking.Entity.Entities;
-
-//namespace HotelBooking.Helpers
-//{
-//    public class TenantMiddleware
-//    {
-//        private readonly RequestDelegate _next;
-
-//        public TenantMiddleware(RequestDelegate next)
-//        {
-//            _next = next;
-//        }
-
-//        public async Task Invoke(
-//            HttpContext context,
-//            IRedisService redis,
-//            IConfiguration configuration)
-//        {
-//            var website = context.Request.Headers["URL"]
-//     .FirstOrDefault()?.Trim().ToLower();
-
-//            //var website = "AMITINFOTECK";
-
-//            if (string.IsNullOrEmpty(website))
-//            {
-//                context.Response.StatusCode = 400;
-//                await context.Response.WriteAsync("URL Header Missing");
-//                return;
-//            }
-
-//            var cacheData =
-//    await redis.GetAsync($"TENANT:{website}");
-
-//            TenantInfo tenant = null;
-
-//            // Redis Miss
-//            if (string.IsNullOrEmpty(cacheData))
-//            {
-//                using var con = new SqlConnection(
-//                    configuration.GetConnectionString("TemplateConnection"));
-
-//                tenant = await con.QueryFirstOrDefaultAsync<TenantInfo>(
-//                        @"SELECT
-//                            ID,
-//                            Website,
-//                            DatabaseName,
-//                            ServerName,
-//                            UserName,
-//                            Password,
-//                            OrganizationCode,
-//                            OrganizationName
-//                        FROM tblOrganization
-//                        WHERE Website = @Website
-//                        AND IsActive = 1
-//                        AND IsDeleted = 0",
-//                        new { Website = website });
-
-//                if (tenant == null)
-//                {
-//                    context.Response.StatusCode = 404;
-//                    await context.Response.WriteAsync("Tenant Not Found");
-//                    return;
-//                }
-
-//                // Save in Redis
-//                await redis.SetAsync(
-//                    $"TENANT:{website}",
-//                    JsonSerializer.Serialize(tenant));
-//            }
-//            else
-//            {
-//                tenant =
-//                    JsonSerializer.Deserialize<TenantInfo>(cacheData);
-//            }
-
-//            context.Items["Tenant"] = tenant;
-
-//            await _next(context);
-//        }
-//    }
-//}
-
-
-
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.Text.Json;
 using Dapper;
 using HotelBooking.DataAccess.Base;
@@ -112,13 +24,13 @@ namespace HotelBooking.Helpers
             var website = context.Request.Headers["URL"]
                 .FirstOrDefault()?.Trim().ToLower();
 
-            //var website = "VISIONTECHSOLUTIONS6";
+            //var website = "29397";
 
             // New PropertyId Header
             var propertyId = context.Request.Headers["PropertyId"]
                 .FirstOrDefault()?.Trim();
 
-            //var propertyId = "o31f2HMvPoq08pr6Q5MEM1ORHSIhjVZm";
+            //var propertyId = "SoLfcrS22qs1fMR4VJWbRYSuAOIImpP6";
 
             // Either URL or PropertyId is required
             if (string.IsNullOrEmpty(website) &&
